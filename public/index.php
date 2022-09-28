15de0c38174871883d778ec6e4207c80288aadff
Santiago Ducos <santidu200@gmail.com>
2022-09-27 23:53:58 -0300

    Response factory

--- public/index.php
+++ public/index.php
@@ -11,30 +11,26 @@ use Lune\Server\PhpNativeServer;
 $router = new Router();
 
 $router->get('/', function (Request $request) {
-    $response = new Response();
-    $response->setHeader('Content-Type', 'application/json');
-    $response->setContent(json_encode(['message' => 'soy la primera ruta']));
-    return $response;
+    return Response::json(['message' => 'soy la primera ruta']);
 });
 
 $router->put('/put', function (Request $request) {
     print('method PUT');
 });
 
+$router->get('/redirect', function (Request $request) {
+    return Response::redirect('/');
+});
+
 
 try {
-
     $server = new PhpNativeServer();
     $request = new Request($server);
     $route = $router->resolve($request);
     $action = $route->action();
     $response = $action($request);
     $server->sendResponse($response);
-    
 } catch (HttpNotFoundException $e) {
-    $response = new Response();
-    $response->setStatus(404);
-    $response->setHeader('Content-Type', 'application/json');
-    // $response->setContent(json_encode(['message' => '404 - Not Found']));
-    $server->sendResponse($response);
+    
+    $server->sendResponse(Response::text('Not Found', 404));
 }
--- src/Http/Response.php
+++ src/Http/Response.php
@@ -7,48 +7,93 @@ class Response
     protected int $status = 200;
     protected array $headers = [];
     protected string | null $content = null;
-    
+
+    // -------------------------------------------------------------
+
     public function status(): int
     {
         return $this->status;
     }
 
-    public function setStatus(int $status): void
+    public function setStatus(int $status): self
     {
         $this->status = $status;
+        return $this;
     }
+
+    // -------------------------------------------------------------
 
     public function headers(): array
     {
         return $this->headers;
     }
 
-    public function setHeader(string $header, string | int $value): void
+    public function setHeader(string $header, string | int $value): self
     {
         $this->headers[strtolower($header)] = $value;
+        return $this;
     }
 
-    public function removeHeader(string $header): void
+    public function removeHeader(string $header): self
     {
         unset($this->headers[strtolower($header)]);
+        return $this;
     }
+
+    // -------------------------------------------------------------
 
     public function content(): ?string
     {
         return $this->content;
     }
 
-    public function setContent(string $content): void
+    public function setContent(string $content): self
     {
         $this->content = $content;
+        return $this;
     }
 
-    public function prepare() {
+    // -------------------------------------------------------------
+
+    public function setContentType(string $value): self
+    {
+        $this->setHeader('Content-Type', $value);
+        return $this;
+    }
+
+    public function prepare(): self
+    {
         if (is_null($this->content)) {
             $this->removeHeader('Content-Type');
             $this->removeHeader('Content-Length');
         } else {
             $this->setHeader('Content-Length', strlen($this->content));
         }
+        return $this;
+    }
+
+    // ----------------------------------------------------------
+
+    public static function json(array $data, int $status = 200): self
+    {
+        return (new self())
+            ->setStatus($status)
+            ->setContentType('application/json')
+            ->setContent(json_encode($data));
+    }
+
+    public static function text(string $text, int $status = 200): self
+    {
+        return (new self())
+            ->setStatus($status)
+            ->setContentType('text/plain')
+            ->setContent($text);
+    }
+
+    public static function redirect(string $url): self
+    {
+        return (new self())
+            ->setStatus(302)
+            ->setHeader('Location', $url);
     }
 }
